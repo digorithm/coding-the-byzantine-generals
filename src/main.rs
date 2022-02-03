f512884d4d3bd94bc8e00fc003be9a86eac6a1ff
Rodrigo Araujo <rod.dearaujo@gmail.com>
2022-02-02 16:14:20 -0800

    Working version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use rand::{prelude::SliceRandom, Rng};
-use std::{
-    borrow::Borrow, cell::RefCell, cmp::Ordering, collections::HashMap, hash::Hash, rc::Rc,
-    slice::SliceIndex,
-};
+use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug, Clone, Default)]
 struct General {
@@ -10,6 +7,7 @@ struct General {
     messages: Vec<Message>,
     is_traitor: bool,
     decision: bool,
+    total_messages_received: usize,
 }
 
 impl General {
@@ -17,51 +15,59 @@ impl General {
         Self {
             id,
             is_traitor,
-            // generals: Vec::new(),
             messages: Vec::new(),
             decision: false,
+            total_messages_received: 0,
         }
     }
 
-    // Idea to try next: global message list that we pass to all generals
     fn receive_order(&mut self, msg: Message, from: usize) {
+        if self.messages.is_empty() {
+            println!(
+                "General #{}'s first order received: {}",
+                self.id, msg.attack
+            );
+            self.decision = msg.attack;
+        }
+
         self.messages.push(msg);
         println!(
             "General #{} receiving order ({}) from commander #{}\n",
             self.id, msg.attack, from,
         );
+        self.total_messages_received += 1;
     }
 
-    fn next_order(&self, msg: Message, idx: usize) -> Message {
-        if self.is_traitor
-        /*&& idx % 2 == 0*/
-        {
-            if msg.attack {
+    fn next_order(&self, idx: usize) -> Message {
+        if self.is_traitor && idx % 2 == 0 {
+            if self.decision {
                 return Message { attack: false };
             } else {
                 return Message { attack: true };
             }
         }
-        msg
+        Message {
+            attack: self.decision,
+        }
     }
 
-    // Given all messages sent to this general, get the consensus
-    fn get_local_majority(&self) -> bool {
+    fn decide(&mut self) {
         let msgs = self
             .messages
             .iter()
             .map(|m| m.attack)
             .collect::<Vec<bool>>();
 
+        println!("Deciding majority for general #{}: {:?}", self.id, msgs);
+
         let attack = msgs.iter().filter(|&attack| *attack).count();
         let retreat = msgs.iter().filter(|&attack| !*attack).count();
 
-        println!("General #{} orders: {:?}", self.borrow().id, msgs);
-
         if attack > retreat {
-            return true;
+            self.decision = true;
+            return;
         }
-        false
+        self.decision = false;
     }
 }
 
@@ -72,109 +78,103 @@ struct Message {
 
 struct OMAlgorithm {
     is_first_commander_loyal: bool,
-    generals: Vec<Rc<RefCell<General>>>,
     original_order: Message,
 }
 
 // Note: the goal is that all _loyal_ general decide on the same plan. It's okay for
 // the traitors to decide on a different plan.
 impl OMAlgorithm {
-    fn om_algorithm(&self, commander: Rc<RefCell<General>>, m: usize, msg: Message) {
-        // If I'm a traitor, I'm gonna send a random order
-        // either the correct or wrong one.
+    fn om_algorithm(
+        &self,
+        generals: &[Rc<RefCell<General>>],
+        commander: Rc<RefCell<General>>,
+        m: usize,
+    ) {
         println!(
             "I'm commander #{}. Sending message: {:?}. m: {}\n",
             commander.as_ref().borrow().id,
-            msg.attack,
+            commander.as_ref().borrow().decision,
             m
         );
 
-        // Experimental
-
-        let mut message_deliveries: Vec<(usize, Message)> = vec![];
-
         if m == 0 {
-            // OM(0) (1):
+            // OM(0):
             // The commander sends his value to every lieutenant.
-            for (idx, general_rc) in self.generals.iter().enumerate() {
-                let general_id = general_rc.as_ref().borrow().id;
-                let commander_id = commander.as_ref().borrow().id;
+            for (idx, general_rc) in generals.iter().enumerate() {
+                let msg_to_relay = commander.as_ref().borrow().next_order(idx);
 
-                if general_id != commander_id {
-                    let mut general = general_rc.borrow_mut();
-                    let msg_to_relay = commander.as_ref().borrow().next_order(msg, idx);
-                    general.receive_order(msg_to_relay, commander_id);
-                    // message_deliveries.push((general_id, msg_to_relay));
-                }
+                let mut general = general_rc.borrow_mut();
+
+                general.receive_order(msg_to_relay, commander.as_ref().borrow().id);
             }
-
-            // OM(0) (2):
-            // Each lieutenant uses the value he receives from the commander
-            // let commander_id = commander.as_ref().borrow().id;
-            // commander.borrow_mut().receive_order(msg, commander_id);
         } else {
             // OM(1) (1):
             // The commander sends his value to every lieutenant.
-            for (idx, general_rc) in self.generals.iter().enumerate() {
-                let general_id = general_rc.as_ref().borrow().id;
-                let commander_id = commander.as_ref().borrow().id;
-                if general_id != commander_id {
-                    let mut general = general_rc.borrow_mut();
+            for (idx, general_rc) in generals.iter().enumerate() {
+                let msg_to_relay = commander.as_ref().borrow().next_order(idx);
 
-                    let msg_to_relay = commander.as_ref().borrow().next_order(msg, idx);
+                let mut general = general_rc.borrow_mut();
 
-                    general.receive_order(msg_to_relay, commander.as_ref().borrow().id);
-                }
+                general.receive_order(msg_to_relay, commander.as_ref().borrow().id);
             }
 
-            // OM(1) (1):
+            // OM(1) (2):
             // For each i, let v(i) be the value Lieutenant i receives
             // from the commander, or else be RETREAT if he
             // receives no value.
             // Lieutenant i acts as the commander in Algorithm OM(m-1)
             // to send the value v(i) to each of the n-2 other lieutenants.
-            for (idx, general_rc) in self.generals.iter().enumerate() {
-                let general = general_rc.as_ref().borrow();
-                let commander_id = commander.as_ref().borrow().id;
-                if general.id != commander_id {
-                    println!("### general #{} acting as commander ###\n", general.id);
+            for general_rc in generals {
+                let next_commander = general_rc.as_ref().borrow();
 
-                    // let next_order = self.next_order(&general, msg);
-                    drop(general);
-                    let msg_to_relay = commander.as_ref().borrow().next_order(msg, idx);
+                println!(
+                    "### general #{} acting as commander ###\n",
+                    next_commander.id
+                );
 
-                    // Sending message v(i) received from the commander
-                    self.om_algorithm(Rc::clone(general_rc), m - 1, msg_to_relay);
+                let mut new_generals: Vec<Rc<RefCell<General>>> = vec![];
+
+                for general in generals {
+                    if general.as_ref().borrow().id != next_commander.id {
+                        new_generals.push(Rc::clone(general));
+                    }
                 }
+                drop(next_commander);
+
+                // Sending message v(i) received from the commander
+                self.om_algorithm(&new_generals, Rc::clone(general_rc), m - 1);
+            }
+
+            // (3) For each i, and each j != i, let v(j) be the value Lieutenant i
+            // received from Lieutenant j in step (2) using Algorithm OM(m - 1),
+            // or else RETREAT if he received no such value.
+            // Lieutenant i uses the value majority (vl, ..., vn-1 ).
+            // Note: honestly, this was the most subtle part of the whole algorithm...
+            // Each general is updating their beliefs based on what they heard so far.
+            for general in generals {
+                println!("deciding for general #{}", general.as_ref().borrow().id);
+                general.borrow_mut().decide();
             }
         }
 
-        // Deliver messages to generals once we've release the borrow_mut
-        // that happens at the recursive step
-
-        // for (id, msg) in message_deliveries {
-        //     self.generals
-        //         .iter()
-        //         .find(|g| g.as_ref().borrow().id == id)
-        //         .unwrap()
-        //         .borrow_mut()
-        //         .messages
-        //         .push(msg);
-        // }
+        println!(
+            "General #{} decision after round OM({}): {}\n",
+            commander.as_ref().borrow().id,
+            m,
+            commander.as_ref().borrow().decision
+        );
     }
 
-    fn get_decisions(&self) {
-        for general in &self.generals {
-            let decision = general.as_ref().borrow().get_local_majority();
-            println!(
-                "General #{} decision: {}\n",
-                general.as_ref().borrow().id,
-                decision
-            );
+    fn get_total_messages(&self, generals: &[Rc<RefCell<General>>]) {
+        let mut total_messages = 0;
+        for general in generals {
+            total_messages += general.as_ref().borrow().total_messages_received;
         }
+
+        println!("total_messages: {:?}\n", total_messages);
     }
 
-    fn was_successful(&self) -> bool {
+    fn was_successful(&self, generals: &[Rc<RefCell<General>>]) -> bool {
         // Here's how Lamport defines success:
         // IC1: All loyal lieutenants obey the same order.
         // IC2: If the commanding general is loyal, then every loyal
@@ -183,10 +183,10 @@ impl OMAlgorithm {
         // Decision of each loyal general
         let mut loyal_decisions = vec![];
 
-        for general in &self.generals {
+        for general in generals {
             let general = general.as_ref().borrow();
             if !general.is_traitor {
-                loyal_decisions.push(general.get_local_majority())
+                loyal_decisions.push(general.decision)
             }
         }
 
@@ -214,61 +214,63 @@ impl OMAlgorithm {
 }
 
 fn main() {
-    // Idea: a good way to explain this is to really focus on the ICs
-    // as in, to really focus on what we're trying to achieve here.
-
+    // Configuration params
     let num_of_generals = 4;
-    let num_of_traitors = 2;
+    let num_of_traitors = 1;
     let m = num_of_traitors;
-
     let num_of_experiments = 10;
 
     for _i in 0..num_of_experiments {
         let mut generals: Vec<Rc<RefCell<General>>> = vec![];
 
+        // Create all generals, set `is_traitor` as false initially
         for i in 0..num_of_generals {
             let g = Rc::new(RefCell::new(General::new(i + 1, false)));
             generals.push(g);
         }
 
-        let g: Vec<_> = generals
+        // Pick `num_of_traitors` random generals and set them as traitors
+        // Note that this could also be the first commanding general
+        generals
             .choose_multiple(&mut rand::thread_rng(), num_of_traitors)
-            .collect();
-
-        for traitor in g {
-            println!("Traitor: General #{}\n", traitor.as_ref().borrow().id);
-            traitor.as_ref().borrow_mut().is_traitor = true;
-        }
+            .for_each(|traitor| {
+                println!("Traitor: General #{}\n", traitor.as_ref().borrow().id);
+                traitor.as_ref().borrow_mut().is_traitor = true;
+            });
 
         let order = Message { attack: true };
 
         let mut om_algorithm = OMAlgorithm {
-            generals,
             is_first_commander_loyal: true,
             original_order: order,
         };
 
+        // Randomly pick first commanding general
         let first_commander = rand::thread_rng().gen_range(0..num_of_generals);
+        let first_commander_rc = Rc::clone(&generals[first_commander]);
 
-        let first_commander_rc = Rc::clone(&om_algorithm.generals[first_commander]);
+        first_commander_rc.borrow_mut().decision = order.attack;
 
-        om_algorithm.generals.remove(first_commander);
+        // Remove first commanding general out of the initial list of lieutenant generals
+        generals.remove(first_commander);
 
         if first_commander_rc.as_ref().borrow().is_traitor {
             om_algorithm.is_first_commander_loyal = false
         }
 
-        om_algorithm.om_algorithm(first_commander_rc, m, order);
+        // Start the algorithm
+        om_algorithm.om_algorithm(&generals, first_commander_rc, m);
 
-        om_algorithm.get_decisions();
+        // Calculate total messages sent for analytical purposes
+        om_algorithm.get_total_messages(&generals);
 
-        let successful = om_algorithm.was_successful();
+        // Check whether it was successful
+        let successful = om_algorithm.was_successful(&generals);
 
         println!("successful: {:?}\n", successful);
-        println!("\n####################################\n");
 
         if !successful {
-            println!("### Found a case where consensus isn't achieved ###");
+            println!("Found a case where consensus isn't achieved\n");
             break;
         }
     }
